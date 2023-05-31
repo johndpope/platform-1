6be8ff6a30811295b380a431b7936fcf3c9de544
Conner Ruhl <connerruhl@me.com>
2023-05-31 12:32:05 -0500

    Copy everything into the new repository from the `next` branch

---
+++ packages/platform-ui/src/index.tsx
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom/client";
+
+import "~/GlobalVariables";
+
+import "./index.css";
+
+ 
+const main = async () => {
+  const root = document.getElementById("app");
+  root &&
+    ReactDOM.createRoot(root).render(
+      <React.StrictMode>
+        Hello, World!
+      </React.StrictMode>
+    );
+};
+
+main();
---
+++ packages/platform-ui/vite.config.ts
@@ -0,0 +1,10 @@
+import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
+import { ViteImageOptimizer as imageOptimizer } from "vite-plugin-image-optimizer";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tsconfigPaths(), react(), visualizer(), imageOptimizer()],
+});
