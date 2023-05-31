9e459a1eb699083de59116c9e45d56eaeb1b023f
kaj <40004347+KAJdev@users.noreply.github.com>
2023-05-31 11:37:30 -0800

    packages

--- packages/sandbox-text-to-image/src/index.tsx
+++ packages/sandbox-text-to-image/src/index.tsx
@@ -1,17 +1,15 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "~/GlobalVariables";
 
 import "./index.css";
 
- 
 const main = async () => {
   const root = document.getElementById("app");
   root &&
     ReactDOM.createRoot(root).render(
-      <React.StrictMode>
-        Hello, World!
-      </React.StrictMode>
+      <React.StrictMode>Hello, World!</React.StrictMode>
     );
 };
 
