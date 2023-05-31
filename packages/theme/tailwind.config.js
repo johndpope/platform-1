aa1cd38a6ccfa48c9b7b7b8b75321e8572b75d3e
kaj <40004347+KAJdev@users.noreply.github.com>
2023-05-31 12:59:23 -0800

    initialize storybook with tailwind

---
+++ packages/theme/tailwind.config.js
@@ -0,0 +1,43 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const plugin = require("tailwindcss/plugin");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  darkMode: ['class', '[data-mode="dark"]'],
+
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  plugins: [
+    require("@tailwindcss/typography"),
+    plugin(({ addVariant }) => {
+      addVariant("group-hover", [".group:hover &", ".group.hover &"]);
+      addVariant("hover", ["&:hover", "&.hover"]);
+      addVariant("active", ["&:active", "&.active"]);
+    }),
+  ],
+
+  theme: {
+    extend: {
+      opacity: {
+        muted: 0.75,
+        "muted-extra": 0.15,
+      },
+
+      colors: {
+        "muted-white": "hsla(0, 0%, 100%, 0.4)",
+        "muted-white-extra": "hsla(0, 0%, 100%, 0.15)",
+      },
+
+      typography: {
+        DEFAULT: {
+          css: {
+            img: {
+              marginTop: 0,
+              marginBottom: 0,
+            },
+          },
+        },
+      },
+    },
+  },
+};
