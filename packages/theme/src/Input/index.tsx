4c4a82323a699878d20080e7a313d6b29fd4a5a0
kaj <40004347+KAJdev@users.noreply.github.com>
2023-06-02 14:07:47 -0800

    input

---
+++ packages/theme/src/Input/index.tsx
@@ -0,0 +1,101 @@
+export type Input = Styleable & {
+  value?: string | number;
+  disabled?: boolean;
+  autoFocus?: boolean;
+  onChange?: (value: string) => void;
+  onNumberChange?: (value: number) => void;
+  placeholder?: string;
+  title?: string;
+  icon?: React.ReactNode;
+  percentage?: boolean;
+  number?: boolean;
+};
+
+export function Input({
+  value,
+  disabled,
+  autoFocus,
+  onChange,
+  onNumberChange,
+  className,
+  placeholder,
+  title,
+  icon,
+  percentage,
+  number
+}: Input) {
+  const [focused, setFocused] = useState(false);
+  const [width, setWidth] = useState(0);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useLayoutEffect(() => {
+    if (percentage) {
+      // calculate scrollWidth of value
+      const tempSpan = document.createElement("span");
+      tempSpan.style.position = "absolute";
+      tempSpan.style.visibility = "hidden";
+      tempSpan.style.whiteSpace = "pre";
+      tempSpan.style.fontSize = "0.875rem";
+      tempSpan.innerHTML = `${value}`
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+      document.body.appendChild(tempSpan);
+      setWidth(tempSpan.getBoundingClientRect().width);
+      document.body.removeChild(tempSpan);
+    }
+  }, [value]);
+
+  return (
+    <div className="flex flex-col gap-1">
+      {title && <h1 className="select-none text-xs opacity-75">{title}</h1>}
+      <div
+        className={classes(
+          "flex max-w-[22rem] items-center gap-2 rounded border px-2 py-1",
+          focused ? "border-black/30" : "border-zinc-300",
+          disabled ? "pointer-events-none opacity-60" : "cursor-text",
+          className
+        )}
+        onClick={() => !disabled && inputRef.current?.focus()}
+      >
+        {icon && (
+          <div className="flex shrink-0 items-center justify-center">
+            {icon}
+          </div>
+        )}
+        <div className="flex max-w-full grow">
+          <input
+            ref={inputRef}
+            autoFocus={autoFocus}
+            disabled={disabled}
+            placeholder={placeholder}
+            value={value}
+            onChange={(e) => {
+              if (number) {
+                const number = parseInt(e.target.value);
+                if (!isNaN(number)) {
+                  onNumberChange?.(number);
+                }
+              } else {
+                onChange?.(e.target.value);
+              }
+            }}
+            style={
+              percentage
+                ? {
+                    maxWidth: `${width}px`
+                  }
+                : undefined
+            }
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+            className={classes(
+              "m-0 min-w-0 max-w-full grow bg-transparent p-0 text-sm focus:outline-none"
+            )}
+          />
+          {percentage && <p className="select-none text-sm opacity-50">%</p>}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ packages/theme/src/Storybook/Input.stories.tsx
@@ -0,0 +1,121 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import "../GlobalVariables";
+
+import { Input } from "../Input";
+import { GridBlocks } from "..";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: "Stability/Input",
+  component: Input,
+  tags: ["autodocs"],
+  argTypes: {
+    value: {
+      description: "The value of the textarea",
+      type: "string"
+    },
+    onChange: {
+      description: "The function to call when the textarea is changed",
+      type: "function"
+    },
+    onNumberChange: {
+      description: "The function to call when the number input is changed",
+      type: "function"
+    },
+    disabled: {
+      description: "Whether the textarea is disabled or not",
+      type: "boolean"
+    },
+    autoFocus: {
+      description: "Whether the textarea is autofocused or not",
+      type: "boolean"
+    },
+    placeholder: {
+      description: "The placeholder text to display inside the textarea",
+      type: "string"
+    },
+    title: {
+      description: "The title of the textarea",
+      type: "string"
+    },
+    number: {
+      description: "Whether the input is a number or not",
+      type: "boolean"
+    },
+    percentage: {
+      description: "Whether the input is a percentage or not",
+      type: "boolean"
+    }
+  }
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    value: "",
+    onChange: () => console.log("Changed!"),
+    placeholder: "Placeholder",
+    title: "Input",
+    icon: <GridBlocks />
+  }
+};
+
+export const Number: Story = {
+  args: {
+    value: "",
+    onNumberChange: () => console.log("Changed!"),
+    placeholder: "Placeholder",
+    title: "Input",
+    icon: <GridBlocks />,
+    number: true
+  }
+};
+
+export const Percentage: Story = {
+  args: {
+    value: "",
+    onNumberChange: () => console.log("Changed!"),
+    placeholder: "Placeholder",
+    title: "Input",
+    icon: <GridBlocks />,
+    percentage: true,
+    number: true
+  }
+};
+
+export const Disabled: Story = {
+  args: {
+    value: "",
+    onChange: () => console.log("Changed!"),
+    placeholder: "Placeholder",
+    title: "Input",
+    icon: <GridBlocks />,
+    disabled: true
+  }
+};
+
+export const Autofocus: Story = {
+  args: {
+    value: "",
+    onChange: () => console.log("Changed!"),
+    placeholder: "Placeholder",
+    title: "Input",
+    icon: <GridBlocks />,
+    autoFocus: true
+  }
+};
+
+export const WithValue: Story = {
+  args: {
+    value: "Value",
+    onChange: () => console.log("Changed!"),
+    placeholder: "Placeholder",
+    title: "Input",
+    icon: <GridBlocks />
+  }
+};
