c58a226cb015b4b57219af7f06dbbed81672596e
Conner Ruhl <connerruhl@me.com>
2023-06-07 15:33:39 -0500

    Update template

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -5,7 +5,7 @@ export function App() {
     <div className="flex h-screen w-screen flex-col">
       <Topbar />
       <iframe
-        src="https://codesandbox.io/p/sandbox/jbe44n?file=/packages/sandbox-text-to-image/src/TextToImage/index.tsx:15,1"
+        src="https://codesandbox.io/p/sandbox/serene-maria-3q0m86?file=%2Fpackages%2Fsandbox-text-to-image%2Fsrc%2FTextToImage%2Findex.tsx%3A15%2C1"
         title="Stability-AI/platform"
         className="h-full w-full"
         allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
